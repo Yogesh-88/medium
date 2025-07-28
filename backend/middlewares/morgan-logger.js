fe43260b774b4c1e5289fef9335f4fa7147a90ab
Yogesh-88 <ydasi793@gmail.com>
2025-07-28 17:13:10 +0530

    add custom format for morgan

--- backend/middlewares/morgan-logger.js
+++ backend/middlewares/morgan-logger.js
@@ -1,6 +1,13 @@
 const morgan = require('morgan');
+const logger = require('../config/logger');
 
-const morganMiddleware = morgan('combined', {
+morgan.token('timestamp', () => new Date().toISOString());
+morgan.token('body', (req) => JSON.stringify(req.body));
+
+const customFormat =
+  ':timestamp :method :url :status :res[content-length] bytes - :response-time ms - User-Agent: :user-agent  -Body:body';
+
+const morganMiddleware = morgan(customFormat, {
   stream: {
     write: (message) => logger.http(message.trim()),
   },
